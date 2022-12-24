7e275a454d15ab991ff07ed091478c2dc7c85d75
GaoYuchenPUMC <106045300+GaoYuchenPUMC@users.noreply.github.com>
2022-12-25 00:51:41 +0800

    Update 1_velocyto_py_workflow.sh

--- code_for_paper_Revision/5_RNA_Velocity/1_velocyto_py_workflow.sh
+++ code_for_paper_Revision/5_RNA_Velocity/1_velocyto_py_workflow.sh
@@ -1,2 +1,40 @@
 ###AD1###
 velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/AD1 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###AD2###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/AD2 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###AD3###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/AD3 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###BM1###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/BM1 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###BM2###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/BM2 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###BM3###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/BM3 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###PM1###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/PM1 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###PM2###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/PM2 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###PM3###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/PM3 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###UC1###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/UC1 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###UC2###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/UC2 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###UC3###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/UC3 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+
+
+
+
