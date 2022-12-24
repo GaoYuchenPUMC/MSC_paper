95705eb9e8d73ce6cad3690ffbe254217476d50c
GaoYuchenPUMC <106045300+GaoYuchenPUMC@users.noreply.github.com>
2022-12-25 00:49:15 +0800

    Create 1_velocyto_py_workflow.sh

---
+++ code_for_paper_Revision/5_RNA_Velocity/1_velocyto_py_workflow.sh
@@ -0,0 +1,2 @@
+###AD1###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/CellRanger_Outs/AD1 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
