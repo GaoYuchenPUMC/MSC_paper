dff03be9145841332d375353e98d1b1745f632ec
GaoYuchenPUMC <106045300+GaoYuchenPUMC@users.noreply.github.com>
2022-12-25 00:56:14 +0800

    Create 3-1_velocyto_py_workflow.sh

---
+++ code_for_paper_Revision/6_SingleCellRNASeq_Young&Aged_BM-MSCs/3-1_velocyto_py_workflow.sh
@@ -0,0 +1,23 @@
+###YoungBM1###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/NC_Revision_YoungAgeBM/CellRanger_Outs/Young_B469 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###YoungBM2###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/NC_Revision_YoungAgeBM/CellRanger_Outs/Young_B537 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###YoungBM3###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/NC_Revision_YoungAgeBM/CellRanger_Outs/Young_B593 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###YoungBM4###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/NC_Revision_YoungAgeBM/CellRanger_Outs/Young_B844 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###AgedBM1###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/NC_Revision_YoungAgeBM/CellRanger_Outs/Aged_B801 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###AgedBM2###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/NC_Revision_YoungAgeBM/CellRanger_Outs/Aged_B842 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###AgedBM3###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/NC_Revision_YoungAgeBM/CellRanger_Outs/Aged_B857 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
+
+###AgedBM4###
+velocyto run10x -m /public/home/gaoyuchen/Velocyto_workflow/maskfile/human_rmsk.gtf /data/NC_Revision_YoungAgeBM/CellRanger_Outs/Aged_B927 /public/home/gaoyuchen/reference_data/Human_reference_GRCh38/refdata-cellranger-GRCh38-1.2.0/genes/genes.gtf
